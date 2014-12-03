8e4ee99ad4d257375910dffa226db0170a76484d
Federico Binda <federico.binda@physics.uu.se>
2014-12-03 13:42:58 +0100

    another_func added

--- func.py
+++ func.py
@@ -6,9 +6,16 @@ def factorial(a):
 		return 1
 	else:
 		return np.prod(np.arange(int(a))+1)
+		
+def another_func(a):
+	"""just return a"""
+	return 0
 	
 def test_factorial():
 	assert(factorial(0) == 1)
 	assert(factorial(1) == 1)
 	assert(factorial(4) == 24)
 	
+def test_another_func():
+	assert(another_func(0) == 0)
+	assert(another_func(2) == 2)
